dde06b5cf5c8a8544e88357dad1a3747d0d4d310
Eugen Guriev <gto200313@gmail.com>
2022-06-26 23:36:14 +0300

    Updates

--- client/src/api/geoAPI.js
+++
@@ -1,8 +0,0 @@
-import { instance } from "./instance"
-
-export const geoAPI = {
-  getGeo: async (cityName, limit) => {
-    const res = await instance.get(`geo/1.0/direct?q=${cityName}&limit=${limit}&appid=c95bd997f720de95dfc717a5eb46fa33`)
-    return res.data
-  }
-}
