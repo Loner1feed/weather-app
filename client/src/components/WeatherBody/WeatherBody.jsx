ceb59c0a50eedefff8b2643e74982a54e36295ea
Eugen Guriev <gto200313@gmail.com>
2022-06-26 19:57:07 +0300

    improved design, small changes and fixes

--- client/src/components/WeatherBody/WeatherBody.jsx
+++ client/src/components/WeatherBody/WeatherBody.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { WeatherIcon } from "../WeatherIcon/WeatherIcon";
+import { style } from "./style/style";
 
 export const WeatherBody = () => {
   const { data } = useSelector((state) => state.weather);
@@ -14,11 +15,24 @@ export const WeatherBody = () => {
   }, [data]);
 
   return (
-    <Box>
+    <Box sx={style.wrapper}>
       {data !== null && (
         <>
-          <h2>{data.weather[0].main}</h2>
-          <WeatherIcon icon={data.weather[0].icon} />
+          <Box sx={style.plate}>
+            <WeatherIcon icon={data.weather[0].icon} />
+            <Box sx={style.info}>
+              <Box component="h2">{data.name + ", " + data.sys.country}</Box>
+              <Box component="span" sx={style.deg}>
+                {Math.round(data.main.temp) + "°"}
+              </Box>
+              <Box component="span" sx={style.desc}>
+                {data.weather[0].main}
+              </Box>
+            </Box>
+          </Box>
+
+          <Box sx={style.plate}></Box>
+          <Box sx={style.plate}></Box>
         </>
       )}
     </Box>
